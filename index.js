b066cabd3f869564cc9cf343eabf2f81717bdc79
Nevin Gilday <nevingilday@gmail.com>
2022-09-03 15:08:42 -0700

    added gui and a test mode

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-
+const dropdown = document.getElementById('testTypeDropdown');
 
 let width = window.innerWidth;
 let height = window.innerHeight;
+let testType = 'reverse';
 
 canvas.width = width;
 canvas.height = height;
@@ -22,20 +23,45 @@ function windowResize() {
 };
   
 window.addEventListener('resize', windowResize);
+document.addEventListener('keydown', logKey);
 draw();
+
+function changedDropdown() {
+    testType = dropdown.value;
+}
 
 //start making an ishaara circle
 function draw() {
-    const colorPalletes = [
+    const normalPalletes = [
          {
              insideColors: ['rgb(220,20,0)', 'rgb(240,20,0)', 'rgb(260,20,0)'],
              outsideColors: ['rgb(110,65,0)', 'rgb(130,65,0)', 'rgb(150,65,0)'],
          },
-        //  {
-        //     insideColors: ['rgb(188,140,15)', 'rgb(113,158,28)', 'rgb(192,146,30)'],
-        //     outsideColors: ['rgb(135,112,37)', 'rgb(237,99,17)', 'rgb(75,126,41)'],
-        // }
+         {
+            insideColors: ['rgb(238,140,108)', 'rgb(217,88,72)'],
+            outsideColors: ['rgb(93,85,56)', 'rgb(166,147,110)'],
+        }
     ];
+
+    const reversePalletes = [
+        {
+           insideColors: ['rgb(178,130,5)', 'rgb(103,148,18)', 'rgb(182,136,20)'],
+           outsideColors: ['rgb(135,112,37)', 'rgb(237,99,17)', 'rgb(75,126,41)'],
+       }
+   ];
+
+    let colorPalletes;
+    switch(testType){
+        case 'normal':
+            colorPalletes = normalPalletes;
+            break;
+        case 'reverse':
+            colorPalletes = reversePalletes;
+            break;
+        default:
+            colorPalletes = reversePalletes;
+            break;
+    }
 
     const plateColors = colorPalletes[Math.floor(Math.random() * colorPalletes.length)];
     const insideColors = plateColors.insideColors;
@@ -165,7 +191,77 @@ function draw() {
         ctx.fill();
         ctx.stroke();
     }
+    if(testType == 'test'){
+        testImage();
+    }
+    
     //ctx.putImageData(myImageData,0,0);
+}
+
+function logKey(keyEvent){
+    if(keyEvent.key == "Enter" || keyEvent.key =="Shift" || keyEvent.key == " "){
+        draw();
+    }
+}
+
+function testImage(){
+    const myImageData = ctx.createImageData(width, height);
+    const minDimension = Math.min(width, height);
+    const plateRadius = minDimension / 4;
+    //get text in ImageData so we can use for later to see if shape overlaps the text
+    //calibrate right font size
+    const numberToAppearInCircle = Math.floor(Math.random() * 90 + 10).toString();
+
+    let fontSize = 0;
+    let textWidth = 0;
+    while(textWidth / (plateRadius * 2) < .8){
+        fontSize++;
+
+        ctx.font = fontSize + 'px Arial';
+        textWidth = ctx.measureText(numberToAppearInCircle).width;
+    }
+
+    ctx.fillStyle = 'rgb(10,10,10)';
+    ctx.strokeStyle = 'rgb(10,10,10)';
+    ctx.fillText(numberToAppearInCircle, width / 2 - textWidth / 2, height / 2 + fontSize / 3.5);
+    const myImageData2 = ctx.getImageData(0,0,width, height);
+
+    ctx.fillStyle = circleBackgroundColor;
+    ctx.strokeStyle = circleBackgroundColor;
+    ctx.arc(width / 2, height / 2, plateRadius, 0, Math.PI * 2, true);
+    ctx.fill();
+    ctx.stroke();
+
+
+    const setColor = (x, y, r, g, b) => {
+        const i = y * (width * 4) + x * 4;
+        myImageData.data[i] = r;
+        myImageData.data[i + 1] = g;
+        myImageData.data[i + 2] = b;
+        myImageData.data[i + 3] = 255;
+      };
+    
+    for(let i =0; i < width; i++){
+        for (let j = 0; j < height; j++){
+            if(myImageData2.data[(j * width + i) * 4] == 10){
+                if(Math.floor(Math.random() * 2) == 0){
+                    setColor(i,j,204,130,85);
+                } else {
+                    setColor(i,j,153,120,89);
+                }
+            }
+            else {
+                if(Math.floor(Math.random() * 2) == 0){
+                    setColor(i,j,209,135,90);
+                } else {
+                    setColor(i,j,158,125,94);
+                }
+            }
+        }
+    }
+
+    ctx.putImageData(myImageData,0,0);
+    
 }
 
 
